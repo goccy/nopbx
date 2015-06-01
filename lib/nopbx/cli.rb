5e5ed3a7a09c4287e0689f8a6a12d689a044b9e6
MasaakiGoshima <masaaki.goshima@mixi.co.jp>
2015-06-01 17:46:23 +0900

    support gen/restore command

--- lib/nopbx/cli.rb
+++ lib/nopbx/cli.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 require "thor"
 require "nopbx/watcher"
+require "nopbx/generator"
 
 module Nopbx
   class CLI < Thor
@@ -13,7 +14,26 @@ module Nopbx
       end
       Nopbx::Watcher.watch pbxproj
     end
-    
+
+    desc "gen", "generate pbxproj directory from your project.pbxproj"
+    def gen(pbxproj=nil)
+      unless pbxproj then
+        puts "nopbx : must be assigned your project.pbxproj"
+        return
+      end
+      Nopbx::Generator.generate_nopbxfiles pbxproj
+      puts "nopbx : generate [pbxproj] successfuly"
+    end
+
+    desc "restore", "restore project.pbxproj from pbxproj directory"
+    def restore(pbxproj=nil)
+      unless pbxproj then
+        puts "nopbx : must be assigned your pbxproj directory"
+        return
+      end
+      Nopbx::Generator.generate_pbxfile pbxproj
+      puts "nopbx : generate [project.pbxproj] successfuly"
+    end
   end
 
 end
